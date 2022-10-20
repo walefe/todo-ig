b1422e02558b285b0713e5b79000c6e8f1f255c3
Walefe <walefedev@gmail.com>
2022-10-19 21:56:46 -0300

    feat: created a page Home with header and form

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import { ButtonForm, Container, Form, Header, InputForm } from "./styles";
+
+import Logo from '../../assets/Logo.svg';
+import { PlusCircle } from "phosphor-react";
+
+export function Home() {
+  return (
+    <Container>
+      <Header>
+        <img src={Logo} alt="logo to-do" />
+      </Header>
+
+      <Form>
+        <InputForm placeholder="Adicione uma nova tarefa" />
+        <ButtonForm>
+          <span>Criar</span>
+          <PlusCircle size={16} weight="bold" />
+        </ButtonForm>
+      </Form>
+    </Container>
+  )
+}
